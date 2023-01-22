d36429098c38fee0fc1d4fb01039be7f3264cecb
Thomas <thnor74@gmail.com>
2023-01-22 20:37:51 +0100

    Added padding

--- components/layout.js
+++ components/layout.js
@@ -3,7 +3,7 @@
 export default function Layout({ children }) {
   return (
     <>
-      <main className="md:container md:mx-auto">{children}</main>
+      <main className="md:container md:mx-auto p-4">{children}</main>
     </>
   )
 }
